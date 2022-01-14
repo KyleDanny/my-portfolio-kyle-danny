6ffbff92337153967952ee17a612c34173eebffc
Kyle Danny <codingkyle92@gmail.com>
2022-01-14 20:09:48 +0200

    repush project and upload

---
+++ app/src/data.js
@@ -0,0 +1,79 @@
+export const projects = [
+    {
+      title: "{COUNT}RIES",
+      subtitle: "MERN Stack",
+      description:
+        "Your very own informative & interactive map of the world!",
+      image: "./images/countries-app.gif",
+      link: "https://reactbootcamp.com",
+    },
+    {
+      title: "::Circuit Speed Dating",
+      subtitle: "Ruby on Rails & Javascript",
+      description:
+        "Making your dating experience safer with face-to-face.",
+      image: "./images/circuit-speed-dating.gif",
+      link: "www.circuit-speed-dating.com",
+    },
+    {
+      title: "Function Fitness App",
+      subtitle: "Ruby and Ruby on Rails",
+      description:
+        "Find your personal trainer wherever you are, and whenever you want!",
+      image: "./images/functional-fitness.gif",
+      link: "https://personal-trainer-kyledanny.herokuapp.com/",
+    },
+    {
+      title: "Epic Todo App",
+      subtitle: "React Hooks",
+      description:
+        "Apples, bananas, milk, bre- ... Oh gee - I need a to do list!",
+      image: "./images/project-4.gif",
+      link: "https://pythonbootcamp.com",
+    },
+  ];
+
+  export const testimonials = [
+    {
+      quote:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+      image: "https://randomuser.me/api/portraits/men/1.jpg",
+      name: "Jesse Hicks",
+      company: "Zoozle",
+    },
+    {
+      quote:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+      image: "https://randomuser.me/api/portraits/men/94.jpg",
+      name: "Ruben Alvarez",
+      company: "Dooble.io",
+    },
+  ];
+  
+  export const skills = [
+    "JavaScript",
+    "Ruby",
+    "NodeJS",
+    "ExpressJS",
+    "REST",
+    "GraphQL",
+    "MongoDB/PostgreSQL",
+    "Ruby on Rails",
+    "React",
+    "Redux",
+    "TypeScript",
+    "Webpack",
+    "HTML",
+    "CSS/SAS/Styled-components",
+    "Visual Studio Code",
+    "Handlebars/Mustache",
+    "Mocha/Jest/Supertest",
+    "Docker",
+    "Git+GitHub",
+    "GitHub Actions",
+    "AWS",
+    "Cloud Platform (MongoDB Atlas, ElephantSQL, Netlify, Heroku, Contentful)",
+    "TDD",
+    "Agile Work Methods",
+  ];
+  
